be484ac227496ab1b871915e9fc5d86995cbbcd8
Naeem Hussain <naeemnh@outlook.com>
2024-05-11 17:03:14 +0400

    fix: using classnames helper function instead of clsx package

--- scenes/DesktopMenu/index.tsx
+++ scenes/DesktopMenu/index.tsx
@@ -1,12 +1,12 @@
 import { Fragment, useState } from 'react';
-import clsx from 'clsx';
 import { IconProps } from '@/types/props.interface';
 import { useWindow } from '@/context/WindowContext';
 import DesktopIcon from '@/components/DesktopIcon'
+import classnames from '@/helpers/classnames';
 import Projects from '../Files/Projects';
 import Skills from '../Files/Skills';
 import Contact from '../Files/Contact';
-import { EnvelopeIcon, FileArchiveIcon, FileIcon, FolderIcon, HomeIcon, ImageIcon, MoreIcon } from '@/icons';
+import { EnvelopeIcon, FileArchiveIcon, FolderIcon, MoreIcon } from '@/icons';
 import styles from './desktopmenu.module.css';
 
 interface Menu {
@@ -20,19 +20,19 @@ interface Menu {
 
 const menu: Menu[] = [
   {
-    label: "Skills.zip",
-    icon: FileArchiveIcon,
-    createWindow: true,
-    content: {
-      component: Skills
-    }
-  },
-  {
     label: "Projects",
     icon: FolderIcon,
     createWindow: true,
     content: {
       component: Projects,
+    }
+  },
+  {
+    label: "Skills.zip",
+    icon: FileArchiveIcon,
+    createWindow: true,
+    content: {
+      component: Skills
     }
   },
   {
@@ -56,13 +56,13 @@ export default function DesktopMenu() {
   const [showOtherThings, setShowOtherThings] = useState<boolean>(false)
 
   return (
-    <nav className={clsx(styles.desktopMenu_wrapper)}>
+    <nav className={styles.desktopMenu_wrapper}>
       {menu.map((item, i) => {
         const onClick = item.createWindow ? (() => createWindow({ children: <item.content.component />, title: item.label })) : () => { setShowOtherThings(!showOtherThings) };
         return (
           <Fragment key={i}>
             <DesktopIcon icon={<item.icon size={30} />} label={item.label} onClick={onClick} />
-            {!item.createWindow && <div className={clsx(styles.desktopMenu_wrapper, styles.otherThings, showOtherThings ? styles.show : '')}>Showing other things</div>}
+            {!item.createWindow && <div className={classnames([styles.desktopMenu_wrapper, styles.otherThings, showOtherThings ? styles.show : ''])}>Showing other things</div>}
           </Fragment>
         )
       })}
