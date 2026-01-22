56b6c5849403eaad4e04e8ba84616bea6235c8f2
Naeem Hussain <naeemnh@outlook.com>
2026-01-23 01:56:27 +0400

    refactor: extract GUI component and add pages index
    - Extract GUI component to separate file for better organization
    - Add pages index for cleaner imports

---
+++ components/pages/gui.tsx
@@ -0,0 +1,48 @@
+import { Env } from "@/config/env";
+import { Section } from "@/components/atoms";
+import { AboutSection, BlogsSection, CanvasAnimation, ContactSection, Dock, HeroSection, WorkSection } from "@/components/organisms";
+import { scrollToSection } from "@/lib/utils";
+
+export const GUI = () => {
+  return (<>
+    {/* Canvas Animation */}
+    <CanvasAnimation />
+
+    {/* Bottom Dock / Taskbar - Fixed */}
+    <Dock />
+
+    {/* </div> */}
+    {/* Main Content - Scrollable Sections */}
+    <main>
+      {/* Home Section */}
+      <Section id="home">
+        <HeroSection onNavigate={(sectionId) => scrollToSection(sectionId)} />
+      </Section>
+
+      {/* Portfolio Section */}
+      <Section id="work">
+        <WorkSection />
+      </Section>
+
+      {/* Blog Section */}
+      {Env.BLOGS_ENABLED && (
+        <Section id="blog">
+          <BlogsSection />
+        </Section>
+      )}
+
+      {/* About Section */}
+      <Section id="about">
+        <AboutSection />
+      </Section>
+
+      {/* Contact Section */}
+      <Section id="contact" className="pb-28 md:pb-36">
+        <div className="px-6 max-w-4xl mx-auto py-24">
+          <ContactSection />
+        </div>
+      </Section>
+    </main>
+  </>
+  );
+};
